88686a594a08f81c3b81b7daf8f8f976ba8ed082
flavio <flavio.bordoni.dev@gmail.com>
2022-03-07 10:34:34 +0100

    set i18n default_locale = :it

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,15 @@ module Bl70
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Comments present in Rails 6:
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.i18n.available_locales = [:en, :it]
+    config.i18n.default_locale = :it
+    config.i18n.fallbacks = true
   end
 end
