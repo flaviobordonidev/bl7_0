0d2e6ee6e14139c3bdee8ed00fa506ac6e3905dc
flavio <flavio.bordoni.dev@gmail.com>
2022-03-11 11:37:40 +0100

    add language field to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  #enum language: [:it, :en]
+  enum language: {it: 0, en: 1}
 end
