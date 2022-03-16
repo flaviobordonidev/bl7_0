fa6fcbaf75ad0509b80f84bef047dfe13d032ad4
flavio <flavio.bordoni.dev@gmail.com>
2022-03-16 14:13:29 +0100

    Activate Devise protection for eg_posts

--- app/controllers/eg_posts_controller.rb
+++ app/controllers/eg_posts_controller.rb
@@ -1,4 +1,5 @@
 class EgPostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_eg_post, only: %i[ show edit update destroy ]
 
   # GET /eg_posts or /eg_posts.json
