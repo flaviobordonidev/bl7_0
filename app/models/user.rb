9b89f2e6bfc42f782a00e79f6507e093d6353c61
flavio <flavio.bordoni.dev@gmail.com>
2022-03-17 09:41:30 +0100

    Add role:enum to table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   #enum language: [:it, :en]
   enum language: {it: 0, en: 1}
-
+  #enum role: [:user, :admin, :moderator, :author]
+  enum role: {user: 0, admin: 1, moderator:2, author:3}
+  
   has_many :eg_posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_153840) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_083629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,8 +44,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_153840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "language", default: 0
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "eg_posts", "users"
