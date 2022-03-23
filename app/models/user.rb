a2bce2b1bf7c6149a0b93f095435ffe8f5852bb7
flavio <flavio.bordoni.dev@gmail.com>
2022-03-23 15:28:11 +0100

    organize models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,34 @@
 class User < ApplicationRecord
+  # == Constants ============================================================
+  
+  # == Extensions ===========================================================
+
+  ## devise
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
+  # == Attributes ===========================================================
+
+  ## enum
   #enum language: [:it, :en]
   enum language: {it: 0, en: 1}
   #enum role: [:user, :admin, :moderator, :author]
   enum role: {user: 0, admin: 1, moderator:2, author:3}
-  
+
+  # == Relationships ========================================================
+
+  ## association one-to-many
   has_many :eg_posts
+
+  # == Validations ==========================================================
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
 end
