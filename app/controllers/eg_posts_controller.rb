2b66ce38147671142fba86c3dfc9622cd9f6b703
flavio <flavio.bordoni.dev@gmail.com>
2022-03-23 12:33:59 +0100

    add Action Text :content field to eg_posts

--- app/controllers/eg_posts_controller.rb
+++ app/controllers/eg_posts_controller.rb
@@ -78,6 +78,6 @@ class EgPostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def eg_post_params
-      params.require(:eg_post).permit(:meta_title, :meta_description, :headline, :incipit, :price, :header_image, :user_id)
+      params.require(:eg_post).permit(:meta_title, :meta_description, :headline, :incipit, :price, :header_image, :content, :user_id)
     end
 end
